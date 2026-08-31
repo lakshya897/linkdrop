d99b7ef4bf1e2af959640db51844a4c649b5a734
lakshya897 <lakshyacodingid@gmail.com>
2026-08-31 20:10:02 +0530

    fix(serverless): import createHash symbol from crypto module

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { randomUUID } from 'crypto';
+import { randomUUID, createHash } from 'crypto';
 
 interface Peer {
   peerId: string;
